917e7d5275165f2eec53459a9c9579d1b78e4546
porovoz <falkon1@list.ru>
2022-11-09 13:50:58 +0300

    homework 08 fixed. Please check!

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,68 @@
-import java.util.Arrays;
+
 
 public class Main {
     public static void main(String[] args) {
         // homework 08 started
 
         // task 1
-        int [] array = new int[3];
+        int[] array = new int[3];
         array[0] = 1;
         array[1] = 2;
         array[2] = 3;
 
-        float [] array1 = {1.57f, 7.654f, 9.986f};
+        float[] array1 = {1.57f, 7.654f, 9.986f};
 
-        int [] array2 = {5, 3, 8};
+        int[] array2 = {5, 3, 8};
 
         // task 2
-        System.out.println(array[0] + ", " + array[1] + ", " + array[2] + "\n"
-                + array1[0] + ", " + array1[1] + ", " + array1[2] + "\n"
-                + array2[0] + ", " + array2[1] + ", " + array2[2]);
-  //            for (int i = 0; i < array[0]; i++) {
-  //          System.out.println(array[0] + ", " + array[1] + ", " + array[2]);
-  //          break;
-  //      }
-  //      for (int i = 0; i < array1[0]; i++) {
-  //          System.out.println(array1[0] + ", " + array1[1] + ", " + array1[2]);
-  //          break;
-  //      }
-  //      for (int i = 0; i < array2[0]; i++) {
-  //          System.out.println(array2[0] + ", " + array2[1] + ", " + array2[2]);
-  //          break;
-  //      }
+        for (int i = 0; i < array.length; i++) {
+            if (i < array.length - 1) {
+                System.out.print(array[i] + ", ");
+            } else
+                System.out.println(array[i]);
+        }
+        for (int i = 0; i < array1.length; i++) {
+            if (i < array.length - 1) {
+                System.out.print(array1[i] + ", ");
+            } else
+                System.out.println(array1[i]);
+        }
+        for (int i = 0; i < array2.length; i++) {
+            if (i < array.length - 1) {
+                System.out.print(array2[i] + ", ");
+            } else
+                System.out.println(array2[i]);
+        }
 
         // task 3
-        System.out.println(array[2] + ", " + array[1] + ", " + array[0] + "\n"
-                + array1[2] + ", " + array1[1] + ", " + array1[0] + "\n"
-                + array2[2] + ", " + array2[1] + ", " + array2[0]);
-  //                  for (int i = 0; i < array[0]; i++) {
-  //                System.out.println(array[2] + ", " + array[1] + ", " + array[0]);
-  //                break;
-  //            }
-  //            for (int i = 0; i < array1[0]; i++) {
-  //                System.out.println(array1[2] + ", " + array1[1] + ", " + array1[0]);
-  //                break;
-  //            }
-  //            for (int i = 0; i < array2[0]; i++) {
-  //                System.out.println(array2[2] + ", " + array2[1] + ", " + array2[0]);
-  //                break;
-  //            }
+        for (int i = array.length - 1; i >= 0; i--) {
+            if (i != 0) {
+                System.out.print(array[i] + ", ");
+            } else
+                System.out.println(array[i]);
+        }
+        for (int i = array1.length - 1; i >= 0; i--) {
+            if (i != 0) {
+                System.out.print(array1[i] + ", ");
+            } else
+                System.out.println(array1[i]);
+        }
+        for (int i = array2.length - 1; i >= 0; i--) {
+            if (i != 0) {
+                System.out.print(array2[i] + ", ");
+            } else
+                System.out.println(array2[i]);
+        }
 
         // task 4
         for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 != 0) {
-                array[i] = array[i] + 1;
+                array[i]++;
             }
-        }
-        System.out.println(array[0] + ", " + array[1] + ", " + array[2]);
+            if (i == array.length - 1) {
+                System.out.print(array[i] + " ");
+            } else
+                System.out.print(array[i] + ", ");
         }
     }
-
+}
